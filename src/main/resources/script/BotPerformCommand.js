42ebc863feee5de42c711735453af2fc458b5374
季楠 <2759278070@qq.com>
2026-01-24 10:11:26 +0800

    feat: 初始化提交

---
+++ src/main/resources/script/BotPerformCommand.js
@@ -0,0 +1,58 @@
+/**
+ * AkariBot
+ *
+ * Bot 执行命令。
+ *
+ * @author 季楠
+ * @since 2026/1/24 09:31
+ */
+
+var users = [];
+var groups = [704109949, 983034842];
+
+function onPluginEnable() {
+    onBotPostEvent();
+}
+
+function onBotPostEvent() {
+    new Listener(BotPostEvent.class)
+        .setExecutor(
+            function (event) {
+                var json = JSON.parse(event.getJson());
+                if (json.post_type !== "message") return;
+
+                var message = json.raw_message;
+                var userId = json.user_id;
+
+                if (!message.startsWith("/执行命令 ")) return;
+                var command = message.substring(6).trim();
+
+                var noPermissionMessage = "无权限执行命令。";
+                var successMessage = "命令执行成功。";
+                var failMessage = "命令执行失败。";
+
+                if (json.message_type === "group") {
+                    var groupId = json.group_id;
+                    if (groups.indexOf(groupId) === -1) return;
+                    if (users.indexOf(userId) === -1) {
+                        BotMessageAPI.sendGroupTextMsg(groupId, noPermissionMessage);
+                        return;
+                    }
+                } else if (json.message_type === "private") {
+                    if (users.indexOf(userId) === -1) {
+                        BotMessageAPI.sendPrivateTextMsg(userId, noPermissionMessage);
+                        return;
+                    }
+                }
+
+                var success = Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command);
+                if (success) {
+                    if (json.message_type === "private") BotMessageAPI.sendPrivateTextMsg(userId, successMessage);
+                    else if (json.message_type === "group") BotMessageAPI.sendGroupTextMsg(groupId, successMessage);
+                } else {
+                    if (json.message_type === "private") BotMessageAPI.sendPrivateTextMsg(userId, failMessage);
+                    else if (json.message_type === "group") BotMessageAPI.sendGroupTextMsg(groupId, failMessage);
+                }
+            }
+        ).register();
+}
--- src/main/resources/script/BotPlayerList.js
+++ src/main/resources/script/BotPlayerList.js
@@ -4,7 +4,7 @@
  * Bot 玩家列表命令。
  *
  * @author 季楠
- * @since 2025/9/19 23:42
+ * @since 2026/1/17 20:48
  */
 
 function onPluginEnable() {
@@ -16,21 +16,28 @@ function onBotPostEvent() {
         .setExecutor(
             function (event) {
                 var json = JSON.parse(event.getJson());
-                if (json.post_type !== "message" || json.message_type !== "group") return;
+                if (json.post_type !== "message") return;
 
                 var message = json.raw_message;
-                if (message === ".玩家列表") {
+                if (message !== "/玩家列表") return;
+
+                var players = Bukkit.getOnlinePlayers();
+                var playerNames = [];
+                for (var i = 0; i < players.length; i++) {
+                    playerNames.push(players.get(i).getName());
+                }
+
+                var playerListMessage = "服务器当前有 " + players.length + " 个玩家在线：" + "\\n" + playerNames.join(", ");
+                var emptyPlayerListMessage = "服务器当前有 0 个玩家在线。";
+
+                if (json.message_type === "private") {
+                    var userId = json.user_id;
+                    if (players.length !== 0) BotMessageAPI.sendPrivateTextMsg(userId, playerListMessage);
+                    else BotMessageAPI.sendPrivateTextMsg(userId, emptyPlayerListMessage);
+                } else if (json.message_type === "group") {
                     var groupId = json.group_id;
-                    var players = Bukkit.getOnlinePlayers();
-                    var playerNames = [];
-                    for (var i = 0; i < players.length; i++) {
-                        playerNames.push(players.get(i).getName());
-                    }
-                    if (players.length !== 0) {
-                        BotMessageAPI.sendGroupTextMsg(groupId, "服务器当前有 " + players.length + " 个玩家在线:\\n" + playerNames.join(", "));
-                    } else {
-                        BotMessageAPI.sendGroupTextMsg(groupId, "服务器当前有 0 个玩家在线");
-                    }
+                    if (players.length !== 0) BotMessageAPI.sendGroupTextMsg(groupId, playerListMessage);
+                    else BotMessageAPI.sendGroupTextMsg(groupId, emptyPlayerListMessage);
                 }
             }
         ).register();
